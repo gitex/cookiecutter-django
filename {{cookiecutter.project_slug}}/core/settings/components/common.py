2fb409ef1be9e91a8c0d4a7d62baf462fcc80767
Дороватовский Евгений <e.dorovatovskyi@benovate.ru>
2022-10-03 02:45:17 +0600

    Added usefully libraries and split settings

---
+++ {{cookiecutter.project_slug}}/core/settings/components/common.py
@@ -0,0 +1,148 @@
+"""
+Django settings for server project.
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/topics/settings/
+For the full list of settings and their config, see
+https://docs.djangoproject.com/en/3.2/ref/settings/
+"""
+
+from typing import Dict, List, Tuple, Union
+from pathlib import Path
+from decouple import config, Csv
+from dj_database_url import parse as db_url
+
+from django.utils.translation import gettext_lazy as _
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+STORAGE_DIR = BASE_DIR / 'storage'
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+SECRET_KEY = config('DJANGO_SECRET_KEY')
+
+# Application definition:
+
+INSTALLED_APPS: tuple[str, ...] = (
+    "django.contrib.admin",
+    "django.contrib.admindocs",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Healthcheck
+    'health_check',
+    'health_check.db',
+    'health_check.cache',
+    'health_check.storage',
+)
+
+MIDDLEWARE: tuple[str, ...] = (
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = "core.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / 'templates']
+        ,
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "core.config.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASES = {
+    "default": config('DATABASE_URL', cast=db_url),
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.1/topics/i18n/
+LANGUAGE_CODE = "en-us"
+TIME_ZONE: str = "{{cookiecutter.timezone}}"
+USE_I18N = True
+USE_TZ = True
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('ru', _('Russian')),
+)
+
+LOCALE_PATHS = (
+    'locale/',
+)
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = STORAGE_DIR / 'static'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+# Media files
+# Media root dir is commonly changed in production
+# (see development.py and production.py).
+# https://docs.djangoproject.com/en/4.1/topics/files/
+MEDIA_URL = '/media/'
+MEDIA_ROOT = STORAGE_DIR / 'media'
+
+
+# Django authentication system
+# https://docs.djangoproject.com/en/4.1/topics/auth/
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
---
+++ {{cookiecutter.project_slug}}/core/settings/enviroments/development.py
@@ -0,0 +1,120 @@
+import socket
+import logging
+
+
+from decouple import config
+
+from core.settings.components.common import INSTALLED_APPS, DATABASE, MIDDLEWARE
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    config('DOMAIN_NAME'),
+    'localhost',
+    '0.0.0.0',  # noqa: S104
+    '127.0.0.1',
+    '[::1]',
+]
+
+
+INSTALLED_APPS += (
+    # Better debug:
+    'debug_toolbar',
+    'nplusone.ext.django',
+
+    # Linting migrations:
+    'django_migration_linter',
+
+    # django-test-migrations:
+    'django_test_migrations.contrib.django_checks.AutoNames',
+    # This check might be useful in production as well,
+    # so it might be a good idea to move `django-test-migrations`
+    # to prod dependencies and use this check in the main `settings.py`.
+    # This will check that your database is configured properly,
+    # when you run `python manage.py check` before deploy.
+    'django_test_migrations.contrib.django_checks.DatabaseConfiguration',
+
+    # django-extra-checks:
+    'extra_checks',
+)
+
+
+# Static files:
+# https://docs.djangoproject.com/en/3.2/ref/settings/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS: list[str] = []
+
+
+MIDDLEWARE += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+    # https://github.com/bradmontgomery/django-querycount
+    # Prints how many queries were executed, useful for the APIs.
+    'querycount.middleware.QueryCountMiddleware',
+)
+
+# https://django-debug-toolbar.readthedocs.io/en/stable/installation.html#configure-internal-ips
+try:  # This might fail on some OS
+    INTERNAL_IPS = [
+        '{0}.1'.format(ip[:ip.rfind('.')])
+        for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+    ]
+except socket.error:  # pragma: no cover
+    INTERNAL_IPS = []
+INTERNAL_IPS += ['127.0.0.1', '10.0.2.2']
+
+
+# nplusone
+# https://github.com/jmcarp/nplusone
+
+# Should be the first in line:
+MIDDLEWARE = (  # noqa: WPS440
+    'nplusone.ext.django.NPlusOneMiddleware',
+) + MIDDLEWARE
+
+
+# Logging N+1 requests:
+NPLUSONE_RAISE = True  # comment out if you want to allow N+1 requests
+NPLUSONE_LOGGER = logging.getLogger('django')
+NPLUSONE_LOG_LEVEL = logging.WARN
+NPLUSONE_WHITELIST = [
+    {'model': 'admin.*'},
+]
+
+
+# django-extra-checks
+# https://github.com/kalekseev/django-extra-checks
+
+EXTRA_CHECKS = {
+    'checks': [
+        # Forbid `unique_together`:
+        'no-unique-together',
+        # Require non empty `upload_to` argument:
+        'field-file-upload-to',
+        # Use the indexes option instead:
+        'no-index-together',
+        # Each model must be registered in admin:
+        'model-admin',
+        # FileField/ImageField must have non empty `upload_to` argument:
+        'field-file-upload-to',
+        # Text fields shouldn't use `null=True`:
+        'field-text-null',
+        # Prefer using BooleanField(null=True) instead of NullBooleanField:
+        'field-boolean-null',
+        # Don't pass `null=False` to model fields (this is django default)
+        'field-null',
+        # ForeignKey fields must specify db_index explicitly if used in
+        # other indexes:
+        {'id': 'field-foreign-key-db-index', 'when': 'indexes'},
+        # If field nullable `(null=True)`,
+        # then default=None argument is redundant and should be removed:
+        'field-default-null',
+        # Fields with choices must have companion CheckConstraint
+        # to enforce choices on database level
+        'field-choices-constraint',
+    ],
+}
+
+
+# Disable persistent DB connections
+# https://docs.djangoproject.com/en/3.2/ref/databases/#caveats
+DATABASES['default']['CONN_MAX_AGE'] = 0
